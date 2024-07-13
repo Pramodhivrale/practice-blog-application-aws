f32814945942b349e56ae26f6d3e448c0a3aa9e0
Admin <hivralepramod4@gmail.com>
2024-07-13 15:32:55 +0530

    Commit

--- src/test/java/com/data/SpringBlogApplicationTests.java
+++ src/test/java/com/data/SpringBlogApplicationTests.java
@@ -2,10 +2,8 @@ package com.data;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(classes = SpringBlogApplication.class)
-@ContextConfiguration
 class SpringBlogApplicationTests {
 
 	@Test
